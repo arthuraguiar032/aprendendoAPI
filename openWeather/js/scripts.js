f8606787d498179a85a36d013f5609080e7ab4ac
aguiar.arthur <aguiar.arthur@graduacao.uerj.br>
2024-06-11 16:26:03 -0300

    adiciona estrutura em html do site de clima

--- openWeather/js/scripts.js
+++ openWeather/js/scripts.js
@@ -1,1 +1,5 @@
 // db5779e6e8b5fc1a68a7dd07232f890d
+
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+
